fc48af031154a0bb3e03f6a17dabd9f6a8f8fe7e
Joshua Jancula <josh.jancula@sebrio.com>
2018-10-14 17:23:38 -0400

    test

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'https://aps-josh.herokuapp.com/api/users');
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200')
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	// res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 	res.header('Access-Control-Allow-Credentials', 'true');
 	next();
 });
