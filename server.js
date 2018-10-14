47ad90a54d324f5bc12d25abc46caec1778ca1a2
Joshua Jancula <josh.jancula@sebrio.com>
2018-10-14 17:16:18 -0400

    try this

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(express.static(path.join(__dirname, '/client/dist/client')));
 app.set('view engine', 'jade');
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin');
+  res.header('Access-Control-Allow-Origin', 'https://aps-josh.herokuapp.com');
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200')
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-	res.header('Access-Control-Allow-Credentials', 'true');
+	// res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	// res.header('Access-Control-Allow-Credentials', 'true');
 	next();
 });
 
