dcc036ed6b7cf4208404118f8786945b6cbd7af6
Joshua Jancula <josh.jancula@sebrio.com>
2018-10-14 18:36:33 -0400

    new db config

---
+++ models/User..js
@@ -0,0 +1,74 @@
+var bcrypt = require('bcrypt-nodejs');
+
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        Username: {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true
+            },
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        FirstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        LastName: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: false,
+        },
+        Role: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: false,
+        },
+        Email: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: false,
+        },
+        Phone: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: true,
+        },
+        Active: {
+            type: DataTypes.BOOLEAN,
+            unique: false,
+            allowNull: false,
+        }
+    });
+
+    User.hook("beforeCreate", function (user) {
+        user.password = bcrypt.hashSync(user.Password, bcrypt.genSaltSync(10), null);
+    });
+
+    User.prototype.validPassword = function (password) {
+        return bcrypt.compareSync(password, this.Password);
+    };
+
+    User.associate = function (models) {
+        User.belongsTo(models.Franchise, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+        User.hasMany(models.Message, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+        User.hasMany(models.Post, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    }
+
+    return User;
+};
+
+
