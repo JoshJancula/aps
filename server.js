93756601e1558d473c34f003a8423b5cb26b00b2
Joshua Jancula <josh.jancula@sebrio.com>
2018-12-31 02:53:30 -0500

    update gitignore;

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(passport.initialize());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ 'extended': 'false' }));
-app.use('/', express.static(path.join(__dirname, '/client/dist/client/index.html')));
+app.use('/', express.static(path.join(__dirname, '/client/dist/client')));
 app.set('view engine', 'jade');
 
 
